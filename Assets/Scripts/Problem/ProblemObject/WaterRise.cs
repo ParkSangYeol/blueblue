e3b9e2beb80d7ce389cd98adc10c57c548565495
dev_ghlee <deonjang2@gmail.com>
2024-10-31 23:34:25 +0900

    Feat: Water Rising problem
    물 block이 올라와 player를 익사시키는 이상현상 구현

---
+++ Assets/Scripts/Problem/ProblemObject/WaterRise.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Problem.Object
+{
+   public class WaterRise : ProblemObject
+   {
+      public GameObject waterBlock;
+      
+      [Tooltip("올라오는 속도 : 1.0 ~ 10.0 설정")]
+      public float waterRiseSpeed;
+
+      private void Start()
+      {
+         base.Start();
+         waterBlock.SetActive(false);
+      }
+
+      private void OnTriggerEnter(Collider other)
+      {
+         if (other.CompareTag("Player"))
+         {
+            ActivePhenomenon();
+         }
+      }
+
+      protected override void ActivePhenomenon()
+      {
+         waterBlock.SetActive(true);
+         StartCoroutine(RiseWater());
+      }
+
+      private IEnumerator RiseWater()
+      {
+         while (waterBlock.transform.localPosition.y < 80f) 
+         {
+            waterBlock.transform.localPosition += new Vector3(0, waterRiseSpeed * Time.deltaTime, 0);
+            yield return null;  
+         }
+         waterBlock.SetActive(false);
+      }
+
+      public override void ResetProblem()
+      {
+         waterBlock.SetActive(false);
+      }
+   }
+}
+ 
+
