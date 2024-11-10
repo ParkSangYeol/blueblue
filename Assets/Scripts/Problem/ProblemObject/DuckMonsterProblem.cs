796e1f7c01b60bbc3dc519c3d8cd6bf89a338c19
Park Sangyeol <tkddufdl77@ajou.ac.kr>
2024-11-10 16:51:35 +0900

    Feat: Toy JumpScare
    오리 괴물이 플레이어를 잡았을 때 나타나는 점프 스케어 1차 구현.

--- Assets/Scripts/Problem/ProblemObject/DuckMonsterProblem.cs
+++ Assets/Scripts/Problem/ProblemObject/DuckMonsterProblem.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.AI;
@@ -14,9 +16,21 @@ namespace Problem.Object
         private NavMeshAgent agent;
         private Transform playerTransform;
         private bool isTracking;
+
+        [SerializeField] 
+        private GameObject jumpScareDuck;
+
+        [SerializeField] 
+        private Camera mainCamera;
+        
+        [SerializeField] 
+        private SFXPlayer sfxPlayer;
+        
+        [SerializeField] 
+        private AudioClip screamSFX;
         
         public UnityEvent onCatchPlayer;
-        
+        private Sequence jumpScareSequence;
         private void Awake()
         {
             startPos = transform.position;
@@ -25,11 +39,19 @@ namespace Problem.Object
             {
                 isTracking = false;
             });
+            onCatchPlayer.AddListener(() =>
+            {
+                StartCoroutine(JumpScare());
+            });
         }
 
         private void Start()
         {
             playerTransform = GameObject.FindWithTag("Player").transform;
+            if (mainCamera == null)
+            {
+                mainCamera = Camera.main;
+            }
             
             base.Start();
         }
@@ -59,11 +81,38 @@ namespace Problem.Object
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Player"))
+            if (isTracking && other.CompareTag("Player"))
             {
                 // 플레이어 잡음
                 onCatchPlayer.Invoke();
             }
+        }
+
+        IEnumerator JumpScare()
+        {
+            Debug.Log("Call");
+            // TODO 플레이어 이동 제한 및 카메라 움직임 제한시키기
+            // TODO 게임 내 모든 조명 비활성화하기
+            // 괴물 오리 오브젝트 이동
+            jumpScareDuck.transform.position = playerTransform.position + new Vector3(0, 2f, 0) + playerTransform.forward * 0.26f;
+            jumpScareDuck.transform.rotation = mainCamera.transform.rotation;
+            // jumpScareDuck.transform.rotation = new Quaternion(0f, jumpScareDuck.transform.rotation.y, 0f, 1f);
+            jumpScareDuck.transform.Rotate(345,180,0,Space.Self);
+            yield return null;
+            // 점프스케어 연출 진행
+            jumpScareSequence = DOTween.Sequence();
+            jumpScareSequence.SetAutoKill(false);
+            jumpScareSequence
+                .Append(jumpScareDuck.transform
+                    .DOMoveY(mainCamera.transform.position.y - 0.17f, 0.5f).SetEase(Ease.OutExpo))
+                .Append(jumpScareDuck.transform
+                    .DORotateQuaternion(playerTransform.rotation * Quaternion.Euler(15, 180, 0), 0.3f)
+                    .SetDelay(1f));
+            // TODO 카메라 흔들림 연출. 현재 CineMachine Brain을 사용해 DOTween 연출이 안먹음. 
+            // SFX 출력
+            SoundManager.Instance.PlaySFX(sfxPlayer, screamSFX, false);
+            yield return null;
+            // TODO 화면 암전 게임 오버 연출 진행.
         }
     }
 }
