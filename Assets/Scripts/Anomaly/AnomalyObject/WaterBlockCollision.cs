2d6849de86541bce9b1c67d4f542831784268be0
dev_ghlee <deonjang2@gmail.com>
2024-12-11 04:53:57 +0900

    Fix: make WaterBlockSpeed increase gradually & add splash sound
    -splash 사운드를 추가해서 물 블럭에 닿을때마다 소리나게 하여 물인줄 인지하도록 만듬
    -lerp를 사용해서 waterBlock의 speed가 점진적으로 증가하게 만듬.

--- Assets/Scripts/Anomaly/AnomalyObject/WaterBlockCollision.cs
+++ Assets/Scripts/Anomaly/AnomalyObject/WaterBlockCollision.cs
@@ -12,53 +12,63 @@ namespace Anomaly.Object
     {
         [SerializeField] private float increaseRate = 0.04f;
         [SerializeField] private float maxIntensity = 0.4f;
-        
+
         private Vignette vignette;
         private CinemachineVolumeSettings CVS;
         private bool isPlayerCollide = false;
         private bool isGameOver = false;
         [SerializeField] private GameObject blackScreen;
         [SerializeField] private SFXPlayer sfxPlayer;
+        [SerializeField] private SFXPlayer sfxPlayer_IN;
+        [SerializeField] private SFXPlayer sfxPlayer_OUT;
         [SerializeField] private AudioClip deadSFX;
+        [SerializeField] private AudioClip splashCameInSFX;
+        [SerializeField] private AudioClip splashCameOutSFX;
         
         private void Start()
         {
+            Init();
             base.Start();
+        }
+
+        private void Init()
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            CVS = player.GetComponentInChildren<CinemachineVolumeSettings>();
+            CVS.m_Profile.TryGet(out vignette);
+            vignette.color.value = Color.black;
+            vignette.intensity.value = 0.2f;
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                CVS = other.GetComponentInChildren<CinemachineVolumeSettings>();
+                //CVS = other.GetComponentInChildren<CinemachineVolumeSettings>();
                 ActivePhenomenon();
+                SoundManager.Instance.PlaySFX(sfxPlayer_IN, splashCameInSFX, false);
             }
         }
+
         private void OnTriggerExit(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 isPlayerCollide = false;
+                SoundManager.Instance.PlaySFX(sfxPlayer_OUT, splashCameOutSFX, false);
                 Debug.Log("escape collision");
             }
         }
-        
+
         protected override void ActivePhenomenon()
         {
-           
-            if (CVS != null && CVS.m_Profile.TryGet(out vignette))
+            if (vignette.color.value != Color.red)
             {
-                if (vignette.color.value != Color.red)
-                {
-                    vignette.color.value = Color.red;
-                }
-                isPlayerCollide = true;
-                StartCoroutine(GetHurt());
+                vignette.color.value = Color.red;
             }
-            else
-            {
-                Debug.Log("Collide");
-            }
+
+            isPlayerCollide = true;
+            StartCoroutine(GetHurt());
         }
 
         private IEnumerator GetHurt()
@@ -79,14 +89,13 @@ namespace Anomaly.Object
                     SceneManager.LoadScene(SceneManager.loadedSceneCount);
                 }
 
-                yield return null;  
+                yield return null;
             }
         }
-        
+
         public override void ResetProblem()
         {
             blackScreen.SetActive(false);
         }
     }
-
 }
--- Assets/Scripts/Anomaly/AnomalyObject/WaterRise.cs
+++ Assets/Scripts/Anomaly/AnomalyObject/WaterRise.cs
@@ -13,8 +13,13 @@ namespace Anomaly.Object
       
       
       [Tooltip("올라오는 속도 : 1.0 ~ 15.0 설정")]
-      public float waterRiseSpeed;
+      public float maxWaterRiseSpeed;
+      
+      [Tooltip("올라오는 가속도: 낮을수록 천천히, 높을수록 빠르게")]
+      public float accelerationFactor = 0.5f;
 
+      private float currentSpeed = 0f; 
+      
       private void Start()
       {
          base.Start();
@@ -38,17 +43,28 @@ namespace Anomaly.Object
 
       private IEnumerator RiseWater()
       {
-         while (waterBlock.transform.localPosition.y < 52f) 
+         Vector3 startPosition = waterBlock.transform.localPosition;
+         Vector3 targetPosition = new Vector3(startPosition.x, 52f, startPosition.z);
+
+         float elapsedTime = 0f; 
+
+         while (waterBlock.transform.localPosition.y < targetPosition.y)
          {
-            waterBlock.transform.localPosition += new Vector3(0, waterRiseSpeed * Time.deltaTime, 0);
-            yield return null;  
+            
+            currentSpeed = Mathf.Lerp(0f, maxWaterRiseSpeed, elapsedTime * accelerationFactor);
+            waterBlock.transform.localPosition += new Vector3(0, currentSpeed * Time.deltaTime, 0);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
          }
-        //waterBlock.SetActive(false);
+         
+         waterBlock.transform.localPosition = targetPosition;
       }
 
       public override void ResetProblem()
       {
          waterBlock.SetActive(false);
+         currentSpeed = 0f;
       }
    }
 }
